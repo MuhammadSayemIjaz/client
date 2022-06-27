8396bf0acf9045e5764bb64789eec9269a1d6f95
tanzeel <75717799+MuhammadSayemIjaz@users.noreply.github.com>
2022-06-27 17:56:09 +0500

    Update ENV Variable

--- src/pages/GetUsers.jsx
+++ src/pages/GetUsers.jsx
@@ -11,7 +11,7 @@ const GetUsers = () => {
   const [singleDoc, setSingleDoc] = useState({});
   const [state, setState] = useState({ userName: "", email: "", age : "" , mobileNo: "" });
 
-  const URL = process.env.REACT_APP_API_END_POINT;
+  const URL = process.env.REACT_APP_END_POINT_API;
 
   useEffect(() => {
     axios
